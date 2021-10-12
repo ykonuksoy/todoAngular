d631b33769858d22791294ce62272f7032ad3c9e
ykonuksoy <yagmurkonuksoy@gmail.com>
2021-10-12 14:54:01 +0300

    list module
    liste ve modül eklendi

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './todoitem';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent {
 
   constructor() { }
+ 
+    private name: string = "Yami";
+    // items = [
+    //   "item 1","item 2","item 3"
+    // ];
+    items: TodoItem[] = [
+      // new TodoItem("kahvaltı", "yes"),
+      // new TodoItem("spor", "yes"),
+      // new TodoItem("alışveriş", "no")
+      {description: "kahvaltı", action: "yes"},
+      {description: "spor", action: "yes"},
+      {description: "alışveriş", action: "no"},
+    ]
 
-  ngOnInit(): void {
-  }
+    getName(){
+      return this.name;
+    }
 
 }
